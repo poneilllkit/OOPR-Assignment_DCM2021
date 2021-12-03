7d3a6f50d6212fb34bcdf89275d9a02cf52beb2e
Pierce O'Neill <l00170246@student.lyit.ie>
2021-12-03 23:10:44 +0000

    adding new files to L00170246_Q6

--- L00170246_Q6/main.tf
+++ L00170246_Q6/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.27"
+      version = "~> 3.5.0"
     }
   }
   required_version = ">= 0.14.9"
@@ -14,6 +14,7 @@ provider "aws" {
 resource "aws_instance" "app_server" {
   ami           = "ami-830c94e3"
   instance_type = "t2.micro"
+
   tags = {
     Name = "L00170246AppServerInstance"
   }
---
+++ L00170246_Q6/variables.tf
@@ -0,0 +1,9 @@
+
+   
+variable "region" {
+}
+variable "instance_name" {
+	description = "Value of the Name tag for the EC2 instance"
+	type		= string
+	default		= "L00170246_Q6_Instance"
+}
